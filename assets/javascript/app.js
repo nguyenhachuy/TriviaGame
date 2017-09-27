b8d9da7dff274b847ab9e4bf51c4e24c9cc9dd2e
Gus Nguyen <nguyenhachuy@gmail.com>
2017-09-27 16:51:48 -0700

    Made questions and answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,40 @@
+//Startup stuff
+$(document).ready = function() {
+
+}
+
+//This is the array of questions and answer expected
+var obj = {
+	"questions" : [
+		"Rick's Main Catchphrase",
+		"Who does Morty like?",
+		"Rick's daughter",
+		"Creature from a box that helps with a specific task",
+		"Character that makes Morty \"uncomfortable\" in a bathroom",
+		"Character that uses the word \"Bitch\" a lot",
+		"Hit song Rick improvises to save Earth",
+		"What does Jerry's new job pay him with?"
+		],
+
+	"answers" : [
+		"Wubalubadubdub",
+		"Jessica",
+		"Beth Smith",
+		"Mr. Meeseeks",
+		"Mr. Jellybean",
+		"Scary Terry",
+		"Get Schwifty",
+		"Pills"
+	],
+
+	"choices" : {
+		"Rick's Main Catchphrase": ["The Whirly Dirly", "TO GET RIGGITY RIGGITY WRECKED SON", "Wubalubadubdub"],
+		"Who does Morty like?": ["Tommy", "Jessica", "Uncle Ben"],
+		"Rick's daughter": ["Beth Smith", "Jerry Smith", "Garzopian"],
+		"Creature from a box that helps with a specific task": ["Mr. Meeseeks", "Evil Morty", "Birdperson"],
+		"Character that makes Morty \"uncomfortable\" in a bathroom": ["Squanchy","Unity","Mr. Jellybean"],
+		"Character that uses the word \"Bitch\" a lot": ["Mr. Poopybutthole","Arthricia","Scary Terry"],
+		"Hit song Rick improvises to save Earth": ["Get Schwifty", "The Vindicators", "Froppy Land"],
+		"What does Jerry's new job pay him with?": ["Noob Noob", "Pills", "Drugs"]
+	}
+}
